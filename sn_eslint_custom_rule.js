60adc0845918e57c607ef01747bf87cab6262440
algimas <algimas@gmail.com>
2023-05-31 09:51:15 +0200

    Create sn_eslint_custom_rule.js

---
+++ sn_eslint_custom_rule.js
@@ -0,0 +1,36 @@
+// custom-rules.js
+module.exports = {
+  "validate-query": {
+    create: function(context) {
+      let glideRecord = false;
+      let addEncodedQueryCalled = false;
+      let isValidEncodedQueryCalled = false;
+
+      return {
+        Identifier(node) {
+          if (node.name === "GlideRecord") {
+            glideRecord = true;
+          }
+          if (glideRecord && node.name === "addEncodedQuery") {
+            addEncodedQueryCalled = true;
+          }
+          if (glideRecord && node.name === "isValidEncodedQuery") {
+            isValidEncodedQueryCalled = true;
+          }
+          if (glideRecord && node.name === "query" && addEncodedQueryCalled && !isValidEncodedQueryCalled) {
+            context.report({
+              node,
+              message: "Ensure isValidEncodedQuery is called after addEncodedQuery and before query."
+            });
+          }
+        },
+        "CallExpression:exit": function(node) {
+          if (node.callee.name === "query") {
+            addEncodedQueryCalled = false;
+            isValidEncodedQueryCalled = false;
+          }
+        }
+      };
+    }
+  }
+};
