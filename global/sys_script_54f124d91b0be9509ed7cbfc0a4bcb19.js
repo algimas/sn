f1f0ace412eb2aa412f3ff8a15f7f91cab8d919e
Algimantas Vaznelis <algimas@gmail.com>
2023-06-13 05:56:15 +0000

    Prettier formatting applied

--- global/sys_script_54f124d91b0be9509ed7cbfc0a4bcb19.js
+++ global/sys_script_54f124d91b0be9509ed7cbfc0a4bcb19.js
@@ -41,7 +41,7 @@
   var gr = new GlideRecord("change_request");
   var Base64 = Packages.org.opensaml.xml.util.Base64;
   var hardCodedSysId = "54f124d91b0be9509ed7cbfc0a4bcb19";
-  current.update();
+  //current.update();
 
   var queryString = "descriptionSTARTSWITHtestttttttttttttttttttttttt";
 
@@ -54,4 +54,4 @@
     inc.setValue("work_notes", "Some text");
     inc.update();
   }
-})(current, previous); //// 2023-06-09 13:06:54// 2023-06-09 13:12:25// 2023-06-09 13:18:14// 2023-06-09 13:59:50// 2023-06-09 14:19:04// 2023-06-09 14:32:37// 2023-06-09 14:40:35// 2023-06-09 14:48:53// 2023-06-09 14:52:53// 2023-06-09 15:31:53// 2023-06-09 15:44:04// 2023-06-09 15:56:15// 2023-06-09 16:12:51// 2023-06-09 16:15:28// 2023-06-12 19:52:19// 2023-06-12 19:56:26// 2023-06-12 20:28:30// 2023-06-12 20:32:14// 2023-06-12 20:41:35// 2023-06-12 21:29:23// 2023-06-12 21:53:55// 2023-06-12 22:06:54
+})(current, previous); // 2023-06-13 05:55:07
