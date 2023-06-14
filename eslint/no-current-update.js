a30082528f8c8fced677013022ed515b46859e9b
algimas <algimas@gmail.com>
2023-06-14 16:40:44 +0200

    Update no-current-update.js

--- eslint/no-current-update.js
+++ eslint/no-current-update.js
@@ -11,7 +11,9 @@ module.exports = {
             recommended: true
         },
         messages: {
-            avoidCurrentUpdate: "Don't call current.update in BR scriptlets. It can cause unintended behavior and performance issues."
+            avoidCurrentUpdate: "Don't call current.update in BR scriptlets. It can cause unintended behavior and performance issues.",
+            metadataNotFound: "Metadata has not been provided.",
+            tableNotFound: "Metadata has been provided but table name has not been found or is different than sys_script - (it is not Business Rule)."
         }
     },
 
@@ -19,16 +21,32 @@ module.exports = {
         const filename = context.getFilename();
         const metadataFilename = process.env.ESLINT_METADATA_PATH;
         let shouldLintFile = false;
+        let messageKey = '';
 
         if (fs.existsSync(metadataFilename)) {
             const metadata = JSON.parse(fs.readFileSync(metadataFilename, 'utf8'));
             const key = Object.keys(metadata)[0]; // first key in the object
             const sysScript = metadata[key]?.record_update?.sys_script;
+            const table = metadata[key]?.record_update?.table;
 
-            shouldLintFile = sysScript && sysScript.sys_class_name === 'sys_script' && sysScript.when === 'after';
+            if (table && table === 'sys_script' && sysScript?.when === 'after') {
+                shouldLintFile = true;
+            } else {
+                messageKey = 'tableNotFound';
+            }
+        } else {
+            messageKey = 'metadataNotFound';
         }
 
         return {
+            Program(node) {
+                if (messageKey) {
+                    context.report({
+                        node: node,
+                        messageId: messageKey
+                    });
+                }
+            },
             MemberExpression(node) {
                 if (shouldLintFile && node.object.name === 'current' && node.property.name === 'update') {
                     context.report({
