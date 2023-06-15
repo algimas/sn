548893193716a5677efcc3edaac6e8c9ae5c58f7
Algimantas Vaznelis <algimas@gmail.com>
2023-06-15 19:45:13 +0000

    Prettier formatting applied for .js and .json

---
+++ global/sys_script_e487bcc8db2be5108412ccd813961946.js
@@ -0,0 +1,10 @@
+(function executeRule(current, previous) {
+  var queryString = "descriptionSTARTSWITHaaaaaaaaag";
+  var inc = new GlideRecord("incident");
+  inc.addEncodedQuery(queryString);
+  inc.query();
+  while (inc.next()) {
+    inc.setValue("work_notes", "Some text");
+    inc.update();
+  }
+})(current, previous); // 2023-06-15 19:43:54
