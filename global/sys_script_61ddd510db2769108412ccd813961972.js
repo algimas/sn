cb1198e1e04c2fa2cabfa434fef0c192b99dfa66
Algimantas Vaznelis <algimas@gmail.com>
2023-06-16 13:32:47 +0000

    Prettier formatting applied for .js and .json

---
+++ global/sys_script_61ddd510db2769108412ccd813961972.js
@@ -0,0 +1,20 @@
+(function executeRule(current, previous) {
+  var queryString = "descriptionSTARTSWITHaaaaaaaaag";
+  var inc = new GlideRecord("incident");
+  inc.addEncodedQuery(queryString);
+  //var validQueryCheck = inc.isValidEncodedQuery(queryString);
+  inc.query();
+  while (inc.next()) {
+    inc.setValue("work_notes", "Some text");
+    if (inc.update()) {
+      //
+    }
+    if (inc.getRowCount() > 0) {
+      //
+    }
+    var varInsideLoop = inc.getValue("sys_id");
+  }
+  current.update();
+  var gr = [];
+  gs.info("test");
+})(current, previous); // 2023-06-16 11:45:21
